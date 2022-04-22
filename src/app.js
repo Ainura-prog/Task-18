5fe34fc27d77d4ecc5ab0d03bd70b85cb0b98660
Ainura-prog <ainura.suerkulova.ss@gmail.com>
2022-04-22 22:40:02 +0600

    First project commit

---
+++ src/app.js
@@ -0,0 +1,37 @@
+import style from "./main.css";
+
+import Untitled from "./images/1.png/2.png";
+
+
+document.querySelector('.images').innerHTML = `
+<img src="${Untitled}"/>`
+
+
+let tab = function() {
+    let tabNav = document.querySelectorAll('.tabs-nav__item'),
+        tabContent = document.querySelectorAll('.tab'),
+        tabName;
+
+    tabNav.forEach(item => {
+        item.addEventListener('click', selectTabNav)
+    });
+
+    function selectTabNav() {
+        tabNav.forEach(item => {
+            item.classList.remove('is-active');
+        });
+        this.classList.add('is-active');
+        tabName = this.getAttribute('data-tab-name');
+        selectTabContent(tabName);
+    }
+
+    function selectTabContent(tabName) {
+        tabContent.forEach(item => {
+            item.classList.contains(tabName) ? item.classList.add('is-active') : item.classList.remove('is-active');
+        })
+    }
+
+};
+
+
+tab();
